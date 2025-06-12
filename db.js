119714ad9bbcc12543ed5177969041fbb4986727
Alessandro Slyusar <alessandro.slyusar22@gmail.com>
2025-06-13 00:33:49 +0200

    refactor: Update db.js to log password type and clean up commented code

--- db.js
+++ db.js
@@ -3,6 +3,16 @@ const { Pool } = pg;
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("Password:", typeof process.env.DB_PASSWORD);
+
+// const db = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT || 5432,
+// });
+
 const db = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -11,3 +21,4 @@ const db = new Pool({
 });
 
 export default db;
+
