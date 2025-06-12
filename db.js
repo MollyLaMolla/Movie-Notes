e342c1806d3b1afddfaa8fb7287a4a06274d0e06
Alessandro Slyusar <alessandro.slyusar22@gmail.com>
2025-06-13 01:46:54 +0200

    refactor: Clean up db.js by removing commented-out Pool configuration and consolidating connection settings

--- db.js
+++ db.js
@@ -5,20 +5,20 @@ dotenv.config();
 
 console.log("Password:", typeof process.env.DB_PASSWORD);
 
-const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432,
-});
-
 // const db = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false // Neon richiede SSL per connessioni remote
-//     }
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT || 5432,
 // });
+
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Neon richiede SSL per connessioni remote
+    }
+});
 
 export default db;
 
